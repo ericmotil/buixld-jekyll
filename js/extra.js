a4e7f8ea53fb1b0276432f7d272ba80cc0bf8eee
Eric <ericmotil@gmail.com>
2014-05-29 09:25:21 -0500

    05-28-14
    Small changes from yesterday and js parallax effect added

--- js/extra.js
+++ js/extra.js
@@ -1,18 +1,32 @@
-$(function() {
-	$('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-				$('html,body').animate({
-					scrollTop: target.offset().top
-				}, 1000);
-				return false;
+$(document).ready(function(){
+
+	// Scroll to div actionnn
+	$(function() {
+		$('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+					return false;
+				}
 			}
+		});
+	});
+
+	// Fade in the Hero yah!
+	$(".hero-quote-text").fadeIn(1500);
+	
+	// Paralaxxxy background thingy
+	$(window).scroll(function(){
+     
+		var scrollPos = $("body").scrollTop();
+		console.log(scrollPos);
+		if (scrollPos > 1) {
+			$(".hero-quote-one").css( "top", -(scrollPos / 2) )
 		}
 	});
-});
 
-$(document).ready(function(){
-    $(".hero-quote-text").fadeIn(1500);
 });
